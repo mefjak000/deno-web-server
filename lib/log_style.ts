ae4233d7022da4123963cefd269beec1b0a90262
Mateusz Trzeciak <trzeciak.3mateusz@gmail.com>
2021-08-26 20:37:18 +0200

    Improvements

--- lib/log_style.ts
+++ lib/log_style.ts
@@ -3,11 +3,11 @@ import {
 } from "./../lib/deps.ts"
 
 export const log_style = {
-    "start": new ServerLog(0, "[SERVER ENGINE]", "#00cc66"),
-    "info": new ServerLog(1, "[SERVER MESSAGE]", "#8f8f8f"),
-    "status": new ServerLog(2, "[SERVER STATUS]", "#0099ff"),
-    "test": new ServerLog(3, "[SERVER TEST]", "#9b34eb"),
-    "resp": new ServerLog(4, "[SERVER RESPONSE]", "#fff"),
-    "warrning": new ServerLog(5, "[SERVER WARRNING]", "#fcc603"),
-    "error": new ServerLog(6, "[SERVER FAIL]", "#f00"),
+    "start": new ServerLog(0, "[  SERVER ENGINE  ]", "#00cc66"),
+    "info": new ServerLog(1, "[  SERVER MESSAGE  ]", "#8f8f8f"),
+    "status": new ServerLog(2, "[  SERVER STATUS  ]", "#0099ff"),
+    "test": new ServerLog(3, "[  SERVER TEST  ]", "#9b34eb"),
+    "resp": new ServerLog(4, "[  SERVER RESPONSE  ]", "#fff"),
+    "warrning": new ServerLog(5, "[  SERVER WARRNING  ]", "#fcc603"),
+    "error": new ServerLog(6, "[  SERVER PANIC  ]", "#f00"),
 }
